762e6217bcf0bd5597a754ec7258d65e81c3a04e
Aleksey <105288834+SanLexNT@users.noreply.github.com>
2023-03-10 10:57:04 +0300

    Init fist JS-file

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+let value1 = 6;
+let value2 = 19;
+console.log("6 * 19 = " + (value1*value2))
+
+let str = "AbRaKadabrA"
+str.toLowerCase();
+str[0].toUpperCase() + str.slice(1);
