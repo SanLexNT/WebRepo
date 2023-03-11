b15b503ebd13bb076f872613a7231561ee5897e4
thesanlex6@gmail.com <thesanlex6@gmail.com>
2023-03-11 10:40:39 +0300

    Start first web-page. Done index.html

--- js/main.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello World");
-let value1 = 6;
-let value2 = 19;
-console.log("6 * 19 = " + (value1*value2))
-
-let str = "AbRaKadabrA"
-str.toLowerCase();
-str[0].toUpperCase() + str.slice(1);
